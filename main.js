1d0572ef3ec68a7e1af4f7d78909ca9328602c28
kokoa0429 <kokoa@ciebus.net>
2021-11-05 08:55:50 +0900

    fix: close

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const express = require("express")
 const puppeteer = require('puppeteer')
 const app = express()
-const api = app.listen(3000, () => console.log("API OK"))
+const api = app.listen(3002, () => console.log("API OK"))
 app.get("/", async (req, res) => {
     const browser = await puppeteer.launch({
         args: [
@@ -16,4 +16,5 @@ app.get("/", async (req, res) => {
     await page.waitForTimeout(Number(req.query.timeout || 0));
     await page.goto(req.query.url);
     res.status(200).send(await page.screenshot({ fullPage: true }));
+    browser.close();
 })
